c05e844042d915b85f79c33e1d52beae358e21dc
Michalina Zeligowska <michalina.zeligowska@gmail.com>
2018-11-03 11:38:03 +0100

    zadanie 2

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 var data = new Date();
 var dataString = "Hello! Today is " + data.getDay() + "." + data.getMonth() + "." + data.getFullYear() + " " + data.getHours() + ":" + data.getMinutes() + ".";
-console.log(dataString);
 
 document.getElementById("dataHello").innerHTML = dataString;
+
+var array = [1,14,15,-31,5,6,129,9,-21,55];
+var min = array[0];
+var max = array[0];
+for(var i = 1; i < array.length; i++) {
+    if(array[i] < min) {
+        min = array[i];
+    }
+    if(array[i] > max) {
+        max = array[i];
+    }
+}
+console.log("min: " + min + ", max: " + max);
